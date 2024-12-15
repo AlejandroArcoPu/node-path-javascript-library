d1332e8a5f1e700008559165ffb6f66556b3b34a
Alejandro Arco <alejandroarpu@gmail.com>
2024-12-15 21:29:40 +0100

    feaT: add mediaquery and icon menu

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+let content_dropdown = document.querySelector(".content_dropdown");
 
 function Book(front_page, title, author, synopsis, pages, read) {
   this.front_page = front_page;
@@ -18,3 +19,15 @@ function addBookToLibrary(front_page, title, author, synopsis, pages, read) {
   let newBook = new Book(front_page, title, author, synopsis, pages, read);
   myLibrary.push(newBook);
 }
+
+function toggleFunction() {
+  content_dropdown.classList.toggle("show");
+}
+
+window.onclick = (event) => {
+  if (!event.target.matches(".button_drop")) {
+    if (content_dropdown.classList.contains("show")) {
+      content_dropdown.classList.remove("show");
+    }
+  }
+};
